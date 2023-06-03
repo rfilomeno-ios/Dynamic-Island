adce8626b2fa59a4ece06e0f94593e4b0887de88
Rodrigo Filomeno <rodrigo.filomeno@radixeng.com.br>
2023-06-03 14:14:39 -0300

    Initial Commit

---
+++ Dynamic Island/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamic Island
+//
+//  Created by Rodrigo Filomeno on 03/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
